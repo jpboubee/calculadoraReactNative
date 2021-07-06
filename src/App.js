4cc7d99ef379d73bd01de43ff01aee97ba5899c8
jpboubee <jp.boubee@hotmail.com>
2021-07-05 21:33:55 -0300

    Ajustes finais

--- src/App.js
+++ src/App.js
@@ -16,16 +16,22 @@ const App = () => {
   const [ponto, setPonto] = useState(true);
 
   const digito = novoNum => {
-    if (novoNum === '0' && numero === '0' || ponto === false && novoNum ==='.') {
-    } else if (numero != '0') {
-      setNumero(numero + novoNum);
-    }else if(ponto === true && novoNum ==='.'){
-         setNumero(numero+novoNum);
-         setPonto(false);
-    }else {
-      setNumero(novoNum);
+    if (novoNum != '.') {
+      if (numero != '0') {
+        setNumero(numero + novoNum);
+      } else {
+        setNumero(novoNum);
+      }
+    } else {
+      if (ponto) {
+        setNumero(numero + novoNum);
+        setPonto(false);
+      } else {
+        setNumero(numero);
+      }
     }
   };
+
   const limpar = () => {
     setNumero(0);
     setOperador(null);
@@ -34,95 +40,87 @@ const App = () => {
     setPonto(true);
   };
 
-  const operacao = (op) => {
-      setNumero2(numero);
-      setOperador(op);
-      setNumero(0);  
-      setPonto(true);
-  }
+  const operacao = op => {
+    setNumero2(numero);
+    setOperador(op);
+    setNumero(0);
+    setPonto(true);
+  };
 
   const resultado = () => {
-      var a = parseFloat(numero2);
-      var b = parseFloat(numero);
-      if(operador==='+'){
-          setResult(a+b)
-      }else if(operador==='-'){
-          setResult(a-b)
-      }else if(operador==='*'){
-          setResult(a*b)
-      }else if(operador==='/'){
-          setResult(a/b)
-      }
-  }
+    var a = parseFloat(numero2);
+    var b = parseFloat(numero);
+    if (operador === '+') {
+      setResult(a + b);
+    } else if (operador === '-') {
+      setResult(a - b);
+    } else if (operador === '*') {
+      setResult(a * b);
+    } else if (operador === '/') {
+      setResult(a / b);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.cabecalho}>
-        <Text style={styles.numero}>{numero2}{operador}{numero}</Text>
+        <Text style={styles.numero}>{numero2}</Text>
+        <Text style={styles.numero}>{operador}</Text>
+        <Text style={styles.numero}>{numero}</Text>
         <Text style={styles.numero}>{result}</Text>
       </View>
       <View style={styles.botoes}>
-      <TouchableOpacity style={styles.botao} onPress={() => limpar()}>
-           <Text style={styles.botaoTexto}> C </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => limpar()}>
-           <Text style={styles.botaoTexto}></Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => limpar()}>
-           <Text style={styles.botaoTexto}></Text>
+        <TouchableOpacity style={styles.botaoC} onPress={() => limpar()}>
+          <Text style={styles.botaoTexto}> AC </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => operacao('+')}>
-           <Text style={styles.botaoTexto}>  +</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('7')}>
-           <Text style={styles.botaoTexto}>  7</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('8')}>
-           <Text style={styles.botaoTexto}>  8</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('9')}>
-           <Text style={styles.botaoTexto}>  9</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => operacao('-')}>
-           <Text style={styles.botaoTexto}>  -</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('4')}>
-           <Text style={styles.botaoTexto}>  4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('5')}>
-           <Text style={styles.botaoTexto}>  5</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('6')}>
-           <Text style={styles.botaoTexto}>  6</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => operacao('*')}>
-           <Text style={styles.botaoTexto}>  *</Text>
+          <Text style={styles.botaoTexto}> +</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => digito('1')}>
-           <Text style={styles.botaoTexto}>  1</Text>
+          <Text style={styles.botaoTexto}> 1</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => digito('2')}>
-           <Text style={styles.botaoTexto}>  2</Text>
+          <Text style={styles.botaoTexto}> 2</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => digito('3')}>
-           <Text style={styles.botaoTexto}>  3</Text>
+          <Text style={styles.botaoTexto}> 3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => operacao('-')}>
+          <Text style={styles.botaoTexto}> -</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('4')}>
+          <Text style={styles.botaoTexto}> 4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('5')}>
+          <Text style={styles.botaoTexto}> 5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('6')}>
+          <Text style={styles.botaoTexto}> 6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => operacao('*')}>
+          <Text style={styles.botaoTexto}> *</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('7')}>
+          <Text style={styles.botaoTexto}> 7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('8')}>
+          <Text style={styles.botaoTexto}> 8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao} onPress={() => digito('9')}>
+          <Text style={styles.botaoTexto}> 9</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => operacao('/')}>
-         <Text style={styles.botaoTexto}>  /</Text>
+          <Text style={styles.botaoTexto}> /</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => limpar()}>
-           <Text style={styles.botaoTexto}></Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao} onPress={() => digito('0')}>
-           <Text style={styles.botaoTexto}>  0</Text>
+        <TouchableOpacity style={styles.botaoZero} onPress={() => digito('0')}>
+          <Text style={styles.botaoTexto}> 0</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.botao} onPress={() => digito('.')}>
-           <Text style={styles.botaoTexto}>.</Text>
+          <Text style={styles.botaoTexto}>.</Text>
         </TouchableOpacity>
 
-
         <TouchableOpacity style={styles.botao} onPress={() => resultado()}>
-           <Text style={styles.botaoTexto}>  =</Text>
+          <Text style={styles.botaoTexto}> =</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -131,19 +129,39 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   padding: 10,
-   justifyContent: 'center',
-   backgroundColor: 'rgba(0,0,0,0.6)',
-   alignItems: 'flex-end',
+    flex: 1,
+    backgroundColor: '#18191A',
   },
   botao: {
-    width: '25%',
-    margin: 'auto',
-    borderWidth: 1,
-    borderColor:'#d3d3d3',
-    padding: '1%'
+   
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+ 
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888',
   },
+  botaoZero: {
+   
+   height: Dimensions.get('window').width / 4,
+   width: Dimensions.get('window').width / 4 * 2,
+   padding: 20,
+
+   textAlign: 'center',
+   borderWidth: 1,
+   borderColor: '#888',
+},
+botaoC: {
+   
+   height: Dimensions.get('window').width / 4,
+   width: Dimensions.get('window').width / 4 * 3,
+   padding: 20,
+
+   textAlign: 'center',
+   borderWidth: 1,
+   borderColor: '#888',
+},
   botoes: {
     display: 'flex',
     flexDirection: 'row',
@@ -151,15 +169,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 30,
     height: '80%',
-    
   },
-  botaoTexto:{
-    textAlign:'center',
+  botaoTexto: {
+    textAlign: 'center',
     color: '#ffffff',
-    fontSize:50,
+    fontSize: 35,
+
   },
   cabecalho: {
-    height: '40%',
+
     justifyContent: 'flex-end',
   },
   numero: {
@@ -168,7 +186,7 @@ const styles = StyleSheet.create({
     fontSize: 50,
     color: '#ffffff',
   },
-  
+  botaoNulo: {},
 });
 
 export default App;
